b3f926b7d48e268bfc63729b9dd4f46c5abe78a4
tmtabor <tmtabor@cloud.ucsd.edu>
2020-09-01 12:56:43 -0700

    Automatic login with cookie

--- library/auth.py
+++ library/auth.py
@@ -1,14 +1,28 @@
 import json
 import requests
+from django.contrib import auth
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
+from django.utils.deprecation import MiddlewareMixin
+from requests import HTTPError
 from rest_framework import authentication
 from rest_framework import exceptions
 
 from library import settings
 
 
-def connect_to_genepattern(username, password):
+def lazily_create_user(username, password_or_token):
+    """Lazily create the Django user object"""
+    try: user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        user = User()
+        user.username = username
+        user.set_password(password_or_token)
+        user.save()
+    return user
+
+
+def login_from_password(username, password):
     """
     Call the GenePattern server to verify credentials
     :param username:
@@ -33,13 +47,7 @@ def connect_to_genepattern(username, password):
         response_payload = json.loads(resp.content)
 
         # Lazily create the Django user object
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            user = User()
-            user.username = username
-            user.set_password(password)
-            user.save()
+        user = lazily_create_user(username, password)
 
         # Return the username
         return (user, response_payload['access_token'])
@@ -48,12 +56,45 @@ def connect_to_genepattern(username, password):
         raise exceptions.AuthenticationFailed('Invalid username or password')
 
 
+def login_from_cookie(request):
+    """Handle login via the GenePattern session cookie"""
+
+    token = request.COOKIES.get('GenePatternAccess')
+    if token is None: return None
+
+    # Attempt to call the username endpoint
+    url = settings.BASE_GENEPATTERN_URL + "/rest/v1/config/user"
+    try:
+        resp = requests.get(url, headers={"Authorization": "Bearer " + token})
+        resp.raise_for_status()
+        resp_json = resp.json()
+        username = resp_json['result']
+
+        # Lazily create the Django user object
+        user = lazily_create_user(username, token)
+
+        # Return the username
+        return { "username": username, "user": user, "token": token, }
+
+    # An error means we can't verify authentication, redirect to login page
+    except HTTPError as e: pass
+
+    # Fall back to logging in via login form if cookie is invalid or not available
+    return None
+
+
 class GenePatternAuthenticationBackend(ModelBackend):
     """
     Base Django authentication using the GenePattern server
     """
 
-    def authenticate(self, *args, **kwargs):
+    def authenticate(self, request, *args, **kwargs):
+
+        # First attempt to authenticate through the GenePattern cookie
+        cookie_login = login_from_cookie(request)
+        if cookie_login is not None:
+            return cookie_login['user']
+
         # Get the login credentials
         if kwargs:
             username = kwargs.pop("username", None)
@@ -63,15 +104,19 @@ class GenePatternAuthenticationBackend(ModelBackend):
 
         # Call GenePattern server
         try:
-            user, token = connect_to_genepattern(username, password)
+            user, token = login_from_password(username, password)
             return user
         except exceptions.AuthenticationFailed:
             return None
 
 
-
 class GenePatternAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
+
+        # First attempt to authenticate through the GenePattern cookie
+        cookie_login = login_from_cookie(request)
+        if cookie_login is not None:
+            return (cookie_login['username'], cookie_login['token'])
 
         # Get the login credentials
         if 'username' in request.POST and 'password' in request.POST:
@@ -81,4 +126,23 @@ class GenePatternAuthentication(authentication.BaseAuthentication):
             return None
 
         # Call GenePattern server
-        return connect_to_genepattern(username, password)
+        return login_from_password(username, password)
+
+
+class AutomaticUserLoginMiddleware(MiddlewareMixin):
+    """Attempt to automatically login if GenePattern cookie is present"""
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        """Check for the cookie if not logged in"""
+        if not AutomaticUserLoginMiddleware._is_user_authenticated(request):
+            user = auth.authenticate(request)
+            if user is None: return            # No cookie or cannot verify, do not do login
+
+            # Log the user in
+            request.user = user
+            auth.login(request, user)
+
+    @staticmethod
+    def _is_user_authenticated(request):
+        user = request.user
+        return user and user.is_authenticated
--- library/settings.py
+++ library/settings.py
@@ -275,6 +275,7 @@ MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'library.auth.AutomaticUserLoginMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
